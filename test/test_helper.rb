9a908edf2666bfde4416dffc3a1bcbf9f84777e9
Diego Mendoza <diego.mendoza@envato.com>
2025-06-20 17:20:14 -0600

    fix on ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require "simplecov"
 
-puts "COVERAGE #{ENV['COVERAGE']}"
-
 SimpleCov.start "rails" do
   add_group "Models", "app/models"
   add_group "Controllers", "app/controllers"
